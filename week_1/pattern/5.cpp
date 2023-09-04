f140fc7ee17cc40ee3a9d065f1be0970f0a3e895
sarthakpanwar <exclusivethought@gmail.com>
2023-09-04 22:53:40 +0530

    first commit

---
+++ week_1/pattern/5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+    cin>>n;
+   	for(int row=0; row<n; row=row+1) {
+		//space
+		for(int col=0; col<n-row-1; col=col+1) {
+			cout <<" ";
+		}
+		//stars
+		for(int col=0; col<row+1; col=col+1) {
+			//print star for first and last col
+			if(col ==0 || col == row + 1 - 1) {
+				cout << "* ";
+			}
+			else {
+				//for every col between first and last
+				//column, print spaces
+				cout << "  ";
+			}
+			
+		}
+		cout << endl;
+	}
+
+    
+    
+	
+
+}
